443b776cf571c06704ac2ba6f461ff831e629335
Andreas Weber <info@weberandreas.eu>
2023-06-10 19:29:10 +0200

    Add rename script

---
+++ bin/rename-etrade.py
@@ -0,0 +1,12 @@
+import os
+import re
+
+pattern = re.compile(r'Brokerage Statement.*(\d{6})\.pdf$')
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+for f in files:
+    match = pattern.match(f)
+    if match:
+        digits = match.group(1)
+        new_name = digits[:4] + "-" + digits[4:6] + "-01-etrade.pdf"
+        os.rename(f, new_name)
+        print(f"Renamed {f} to {new_name}")
